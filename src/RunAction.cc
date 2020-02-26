7160b43060032952a0df80d343e91a823f34cc3d
Takumi Yamaga <takumi.yamaga@riken.jp>
2020-02-26 13:54:16 +0900

    implemented setting for random seed (from Time)

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -30,6 +30,8 @@
 #include "RunAction.hh"
 #include "Analysis.hh"
 
+#include "time.h"
+
 #include "G4Run.hh"
 #include "G4RunManager.hh"
 #include "G4UnitsTable.hh"
@@ -113,10 +115,13 @@ RunAction::~RunAction()
 
 void RunAction::BeginOfRunAction(const G4Run* /*run*/)
 { 
-  G4long seed = 1;
-  HepRandom::setTheSeedseed(seed,4);
+  G4long random_seed  = time(NULL);
+  G4int random_luxury = 5;
+  CLHEP::HepRandom::setTheSeed(random_seed,random_luxury);
+
   //inform the runManager to save random number seed
   G4RunManager::GetRunManager()->SetRandomNumberStore(true);
+  G4RunManager::GetRunManager()->SetRandomNumberStoreDir("./rndm/");
 
   // Get analysis manager
   auto analysisManager = G4AnalysisManager::Instance();
